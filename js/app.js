b290e9b83a33890aaf50b0c3d9cc01bf28682427
Ali Sbane <ali.sbane.dev@gmail.com>
2022-02-26 23:01:27 +0100

    header and hero section for desktop screens

--- js/app.js
+++ js/app.js
@@ -15,26 +15,63 @@
     let linksWithSubMenus = $(`a[data-next-sibling-submenu="true"`);
 
     // hum Icon and menu opening
-    humIcon.click(function () {
-      humIcon.toggleClass("open");
-      nav.toggleClass("open");
-    });
 
-    // submenus showing
-    linksWithSubMenus.each(function () {
-      $(this).click(function () {
-        const currentSubmenu = $(this).next();
-        const currentSubmenuHeight = $(currentSubmenu)[0].scrollHeight;
-
-        $(this).toggleClass("open");
-        $(currentSubmenu).toggleClass("open");
-
-        if ($(currentSubmenu).hasClass("open")) {
-          $(currentSubmenu).css("height", `${currentSubmenuHeight + (17 * 2)}px`);
-        } else {
-          $(currentSubmenu).removeAttr("style");
-        }
+    if (isMobileDevice()) {
+      // check if the device is mobile
+      humIcon.click(function () {
+        humIcon.toggleClass("open");
+        nav.toggleClass("open");
       });
-    });
+    }
+
+    if (isMobileDevice()) {
+      // check if the device is mobile
+      // submenus showing
+      linksWithSubMenus.each(function () {
+        $(this).click(function () {
+          const currentSubmenu = $(this).next();
+          const currentSubmenuHeight = $(currentSubmenu)[0].scrollHeight;
+
+          $(this).toggleClass("open");
+          $(currentSubmenu).toggleClass("open");
+
+          if ($(currentSubmenu).hasClass("open")) {
+            $(currentSubmenu).css(
+              "height",
+              `${currentSubmenuHeight + 17 * 2}px`
+            );
+          } else {
+            $(currentSubmenu).removeAttr("style");
+          }
+        });
+      });
+    } else {
+      // hover event
+      linksWithSubMenus.each(function () {
+        $(this).hover(function () {
+          const currentSubmenu = $(this).next();
+          const currentSubmenuHeight = $(currentSubmenu)[0].scrollHeight;
+
+          $(this).toggleClass("open");
+          $(currentSubmenu).toggleClass("open");
+
+          if ($(currentSubmenu).hasClass("open")) {
+            $(currentSubmenu).css(
+              "height",
+              `${currentSubmenuHeight + 17 * 2}px`
+            );
+          } else {
+            $(currentSubmenu).removeAttr("style");
+          }
+        });
+      });
+    }
   }
 })();
+
+function isMobileDevice() {
+  if (window.matchMedia("(max-width: 767px)").matches) {
+    return true;
+  }
+  return false;
+}
